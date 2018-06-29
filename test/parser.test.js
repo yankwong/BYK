5e544e8ae5e24af87a2344564638e601fc2313bb
Andy Kwong <Andy.Kwong@hunterindustries.com>
2018-06-29 14:38:07 -0700

    initial setup for project w. TDD

---
+++ test/parser.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//const jsforms = require('../src/1_first-form');
+const assert = require('chai').assert;
+
+describe('Basic Parser', function () {
+    it('should say "Hello!" by default');
+});
