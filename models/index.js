fbfc7bff1cc71a77dcf29e391a6f097e8943d919
Yan Kwong <yankwong@yans-air.hunterindustries.com>
2018-07-02 08:59:05 -0700

    basic sequalize setup

---
+++ models/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+var basename  = path.basename(__filename);
+const Sequelize = require ('sequelize');
+
+var env    = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config/config.js')[env];
+
+let sequelize;
+
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.user, config.password, config);
+}
+
+
+var db = {};
+
+// Read all the files in this directory and import them as models
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
