6c5ca7d85fbb77997ef56a2ab0347707441eb3b0
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2018-07-07 16:30:58 -0700

    removed unit tests

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const sinon = require('sinon');
 const assert = require('chai').assert;
 
 const userData = require('../services/userData');
@@ -9,5 +8,6 @@ const config = require('../config/config')['test'];
 describe('userData', function () {
 
     it('should return an object when there is no error');
+
     it('should return error when encountered error');
 });
