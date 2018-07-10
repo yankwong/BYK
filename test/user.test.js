531b6b1298f421f5f54b51723faed5b034c35dfa
Yan Kwong <yantkwong@gmail.com>
2018-07-09 21:56:04 -0700

    added test to userData

--- test/user.test.js
+++ test/user.test.js
@@ -1,13 +1,32 @@
 'use strict';
 
 const assert = require('chai').assert;
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const db = require('../models');
 
 const userData = require('../services/userData');
-const config = require('../config/config')['test'];
 
 describe('userData', function () {
 
-    it('should return an object when there is no error');
+    it('should return an object when there is no error', (done) => {
+        let queryResult = [];
+        let stub = sinon.stub(db.user, 'findAll');
+        stub.resolves(queryResult);
+        
+        // var callback = sinon.spy();
+
+        let callback = sinon.spy();
+
+        userData.getAllUsers(callback);
+
+        // callback's first argument should deepEqual queryResult
+        expect(queryResult).to.deep.equal(callback.args);
+
+        done();
+
+    });
 
     it('should return error when encountered error');
 });
