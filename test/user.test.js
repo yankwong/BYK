92b15e6950eedc4d141d543aa1fb16ca77eb2f34
Yan Kwong <yantkwong@gmail.com>
2018-07-10 18:26:21 -0700

    adjusted unit tests, added test for err case

--- test/user.test.js
+++ test/user.test.js
@@ -2,31 +2,43 @@
 
 const assert = require('chai').assert;
 const expect = require('chai').expect;
-const sinon = require('sinon');
+let sinon = require('sinon');
 
 const db = require('../models');
 
 const userData = require('../services/userData');
 
 describe('userData', function () {
+    let stubFindAll;
 
-    it('should return an object when there is no error', (done) => {
-        let queryResult = [];
-        let stub = sinon.stub(db.user, 'findAll');
-        stub.resolves(queryResult);
-        
-        // var callback = sinon.spy();
-
-        let callback = sinon.spy();
-
-        userData.getAllUsers(callback);
-
-        // callback's first argument should deepEqual queryResult
-        expect(queryResult).to.deep.equal(callback.args);
-
-        done();
-
+    beforeEach(() => {
+        stubFindAll = sinon.stub(db.user, 'findAll');
     });
 
-    it('should return error when encountered error');
+    afterEach(() => {
+        stubFindAll.restore();
+    });
+
+    it('should pass in queryResult to callback when there is no error', (done) => {
+        let queryResult = ['whatever1', 'whatever2'];
+
+        stubFindAll.resolves(queryResult);
+
+        userData.getAllUsers((...args) => {
+            //assertion goes here, when the callback actually get called
+            expect(queryResult).to.deep.equal(args[1]);
+
+            done();
+        });
+    });
+
+    it('should pass null to callback when encountered error', (done) => {
+        stubFindAll.rejects('TypeError');
+
+        userData.getAllUsers((...args) => {
+            expect(args[1]).to.be.null;
+
+            done();
+        });
+    });
 });
